6c40b12cdfe09ee9ce8f46a73a79532f58e898b8
[dyad] <ilayabharathi8888@gmail.com>
2025-10-03 18:20:00 +0530

    [dyad] Removed the "Gym Workouts" section from the Workout Log page. - wrote 1 file(s) + extra files edited outside of Dyad

--- src/pages/WorkoutLog.tsx
+++ src/pages/WorkoutLog.tsx
@@ -11,16 +11,14 @@ const WorkoutLog = () => {
       <p className="text-muted-foreground">Log your exercises and track your fitness progress.</p>
 
       <Tabs defaultValue="home" className="w-full">
-        <TabsList className="grid w-full grid-cols-2">
+        <TabsList className="grid w-full grid-cols-1"> {/* Changed to grid-cols-1 */}
           <TabsTrigger value="home">Home Workouts</TabsTrigger>
-          <TabsTrigger value="gym">Gym Workouts</TabsTrigger>
+          {/* Removed TabsTrigger for Gym Workouts */}
         </TabsList>
         <TabsContent value="home">
           <WorkoutTypeSection type="home" />
         </TabsContent>
-        <TabsContent value="gym">
-          <WorkoutTypeSection type="gym" />
-        </TabsContent>
+        {/* Removed TabsContent for Gym Workouts */}
       </Tabs>
     </div>
   );
